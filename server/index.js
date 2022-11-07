f4c71a03bde039be6c04921b72359bb9827eef6d
Priyanka Jain <jain.prii.yanka@gmail.com>
2022-11-07 01:29:17 -0500

    push to heroku

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
+const path = require('path');
 // const cors = require("cors");
 
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(require("./routes/record"));
-// get driver connection
-
-const port = process.env.PORT || 5001;
 // const dbo = require("./startup/conn");
 // require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/conn')();
 
- 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname,'../client/build')));
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  })
+}
+
+const port = process.env.PORT || 5001;
+
 const server = app.listen(port, () => {
   // perform a database connection when server starts
   // dbo.connect('zest'); // need to maybe change this parameter.
